1f0eb0fe1a79849732a9c714db4918e69ccb948d
Souha Aknouche <aknouchechaima00@gmail.com>
2024-11-30 13:18:37 +0100

    .

--- fastAPI/routers/fnctsRoute.py
+++ fastAPI/routers/fnctsRoute.py
@@ -1,51 +1,26 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from typing import List
 from models.fncts import PasswordFeature
+import controllers.controllersFcts as controller  
 
 router = APIRouter()
 
 @router.post("/create", response_model=PasswordFeature)
 async def create_feature(feature: PasswordFeature):
-
-    try:
-        await feature.insert()
-        return feature
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    return await controller.create_feature(feature)
 
 @router.get("/list", response_model=List[PasswordFeature])
 async def list_features():
-
-    features = await PasswordFeature.find_all().to_list()
-    return features
+    return await controller.list_features()
 
 @router.get("/{feature_id}", response_model=PasswordFeature)
 async def get_feature(feature_id: str):
-  
-    feature = await PasswordFeature.get(feature_id)
-    if not feature:
-        raise HTTPException(status_code=404, detail="Fonctionnalité non trouvée")
-    return feature
+    return await controller.get_feature(feature_id)
 
 @router.put("/modifie/{feature_id}", response_model=PasswordFeature)
 async def update_feature(feature_id: str, updated_data: PasswordFeature):
-
-    feature = await PasswordFeature.get(feature_id)
-    if not feature:
-        raise HTTPException(status_code=404, detail="Fonctionnalité non trouvée")
-
-    for field, value in updated_data.dict().items():
-        setattr(feature, field, value)
-
-    await feature.save()
-    return feature
+    return await controller.update_feature(feature_id, updated_data)
 
 @router.delete("/delete/{feature_id}")
 async def delete_feature(feature_id: str):
-
-    feature = await PasswordFeature.get(feature_id)
-    if not feature:
-        raise HTTPException(status_code=404, detail="Fonctionnalité non trouvée")
-
-    await feature.delete()
-    return {"message": "Fonctionnalité supprimée avec succès"}
+    return await controller.delete_feature(feature_id)
--- fastAPI/routers/userRoute.py
+++ fastAPI/routers/userRoute.py
@@ -1,47 +1,18 @@
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from models.users import Userhexvault, UserCreate,UserLogin
-
+from models.users import Userhexvault, UserCreate, UserLogin
+import controllers.controllerUser as controlleuser
 
 router = APIRouter()
 
+
 @router.post("/register/")
-async def create_user(user: UserCreate):
+async def register_user(user: UserCreate):
+    return await controlleuser.create_user(user)
 
-    try:
-      
-        existing_user_email = await Userhexvault.find_one(Userhexvault.email == user.email)
-        existing_user_username = await Userhexvault.find_one(Userhexvault.username == user.username)
-        
-        if existing_user_email:
-            raise HTTPException(status_code=400, detail="Un utilisateur avec cet email existe déjà")
-        
-        if existing_user_username:
-            raise HTTPException(status_code=400, detail="Ce nom d'utilisateur est déjà pris")
-        
-      
-        new_user = await Userhexvault.create_user(
-            nom=user.nom,
-            prenom=user.prenom,
-            username=user.username,
-            email=user.email,
-            password=user.password
-        )
-        
-        return {"message": "Utilisateur créé avec succès", "user_id": str(new_user.id)}
-    
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
 
+#Rajouter l'autentification
 @router.post("/login")
-async def login(data: UserLogin):
-    user = await Userhexvault.find_one(Userhexvault.username == data.username)
-    
-    if not user:
-        raise HTTPException(status_code=404, detail="Utilisateur non trouvé")
-    
- 
-    if not user.verify_password(data.password):
-        raise HTTPException(status_code=400, detail="Mot de passe incorrect")
-    
-    return {"access_token": "token_factice", "token_type": "bearer"}
+async def login_user(form_data: UserLogin):
+    return await controlleuser.login_user(form_data)
+
