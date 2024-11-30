7e0dbb120ffeebd78ec756ec5926c6f3275ed3b5
Meriem Fed <meriemfedaoui1@gmail.com>
2024-11-30 20:17:09 +0100

    .

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import '../styles/Sections.css';
 
 function Hero() {
   return (
     <div>
-        <h1></h1>
+        <div className='slogan'>
+            <h1>Crack, Test, Encrypt</h1>
+            <h1>All in One Vault</h1>
+
+        </div>
+        <div className='serv'>
+            <label >encrypt</label>
+            <label >decrypt</label>
+            <label >attack simulation</label>
+            <label >test your password</label>
+            <label >time prediction</label>
+        </div>
+        
     </div>
   )
 }
--- frontend/src/pages/Section1.jsx
+++ frontend/src/pages/Section1.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import Background from '../components/Background';
 import '../styles/Sections.css'
+import Hero from '../components/Hero';
 
 function Section1() {
   return (
     <div className='section-1'>
        <div className='bg'>
         <Background />
+       
+
       </div>
-      
+       <div className='main-text'>
+        <Hero/>
+       </div>
     </div>
   )
 }
