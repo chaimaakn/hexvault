cc7b21fd1fb58dfdf4c6083b1381de1d23c1e19e
Haniaa567 <154731128+Haniaa567@users.noreply.github.com>
2024-12-25 15:02:15 +0100

    .

--- frontend/src/utils/Keycloak.js
+++ frontend/src/utils/Keycloak.js
@@ -3,9 +3,9 @@ import Keycloak from 'keycloak-js';
 // Keycloak configuration options
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8080', // URL to your Keycloak server
-  realm: 'myRealm', // Name of your Keycloak realm
-  clientId: 'Client1', // Your client ID in Keycloak
+  url: 'http://localhost:8080/', // URL to your Keycloak server
+  realm: 'HEXVAULT', // Name of your Keycloak realm
+  clientId: 'myClient', // Your client ID in Keycloak
 });
 
 
